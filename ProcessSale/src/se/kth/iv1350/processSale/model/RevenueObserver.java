2771930765b6851b2799481f4f4b9d9d9f65f462
William Axbrink <Williamaxb@gmail.com>
2020-05-11 18:24:29 +0200

    adding comments

--- ProcessSale/src/se/kth/iv1350/processSale/model/RevenueObserver.java
+++ ProcessSale/src/se/kth/iv1350/processSale/model/RevenueObserver.java
@@ -1,8 +1,12 @@
 package se.kth.iv1350.processSale.model;
 
+/**
+ * An interface class for an observer that observes the revenue of each sale.
+ */
 public interface RevenueObserver {
     /**
-     *
+     * A method that performs an action and adds the new payment to the observer.
+     * @param money The money being added.
      */
     void newPayment(Money money);
 }
